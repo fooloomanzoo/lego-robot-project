8088fd307121d06aed3e831e22ff327b1633dbd0
kevkal <kevkal@hotmail.de>
2017-04-19 14:25:13 +0200

    edit HelloWorld now

---
+++ Uebung1/src/HelloWord.java
@@ -0,0 +1,11 @@
+import lejos.nxt.Button;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+        Button.waitForAnyPress();
+	}
+
+}
