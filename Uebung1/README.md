c396b0810a93c215544b49dc22c5a7a1c0aeba3c
fooloomanzoo <hannes-b@web.de>
2017-04-20 02:01:02 +0200

    Haus vom Nikolaus, alternative Rotationsfunktion mit Rotation um Mittelpunkt der Achse

--- Uebung1/README.md
+++ Uebung1/README.md
@@ -1,1 +1,9 @@
--- Ãœbung 1
+-- Übung 1
+
+--- Bestimmung des Radumfangs
+
+---- Messungen
+
+--- Formel zur Umrechnung einer Distanz in einen Rotationswinkel
+
+--- Formel zur Umrechnung einer Distanz in einen Rotationszeit
--- Uebung1/src/MoveTests.java
+++ Uebung1/src/MoveTests.java
@@ -13,23 +13,63 @@ public class MoveTests {
 
 	public static void main(String[] args) {
 
+//		/*******************************************************/
+//		System.out.println("Druecke Startbutton!");
+//		Button.waitForAnyPress();
+//
+//		System.out.println("Geradeaus Fahren");
+//		moveMilimetersBlocking(50, SPEED_FAST); // 5 cm nach vorn
+//		
+//		/*******************************************************/
+//		System.out.println("Druecke Startbutton!");
+//		Button.waitForAnyPress();
+//		
+//		System.out.println("Drehung um eine Achse");
+//		rotateDegreeBlocking(90, SPEED_SLOW);   // 90° nach rechts
+//		
+//		/*******************************************************/
+//		System.out.println("Druecke Startbutton!");
+//		Button.waitForAnyPress();
+//		
+//		System.out.println("Drehung um Mittelachse");
+//		rotateSimultaneDegreeBlocking(45, SPEED_SLOW); // 90° nach rechts
+		
+		/*******************************************************/
 		System.out.println("Druecke Startbutton!");
 		Button.waitForAnyPress();
-
-		/*******************************************************/
-		System.out.println("Geradeaus Fahren");
-		moveMilimetersBlocking(50, SPEED_FAST); // 5 cm nach vorn
 		
-		System.out.println("Druecke Startbutton!");
-		Button.waitForAnyPress();
-		
-		/*******************************************************/
-		System.out.println("Drehen");
-		rotateDegreeBlocking(90, SPEED_SLOW);   // 90° nach rechts		
+		System.out.println("Nikolaus");
+		int speed = SPEED_MEDIUM;
+		// Schritt 1
+		moveMilimetersBlocking(500, speed);
+		// Schritt 2
+		rotateDegreeBlocking(135, speed);
+		moveMilimetersBlocking( (int) (500 * Math.sqrt(2)), speed);
+		// Schritt 3
+		rotateDegreeBlocking(-45, speed);
+		moveMilimetersBlocking(500, speed);
+		// Schritt 4
+		rotateDegreeBlocking(-45, speed);
+		moveMilimetersBlocking( (int) (500 / Math.sqrt(2)), speed);
+		// Schritt 5
+		rotateDegreeBlocking(-90, speed);
+		moveMilimetersBlocking( (int) (500 / Math.sqrt(2)), speed);
+		// Schritt 6
+		rotateDegreeBlocking(-135, speed);
+		moveMilimetersBlocking(500, speed);
+		// Schritt 7
+		rotateDegreeBlocking(135, speed);
+		moveMilimetersBlocking( (int) (500 * Math.sqrt(2)), speed);
+		// Schritt 8
+		rotateDegreeBlocking(-135, speed);
+		moveMilimetersBlocking(500, speed);
+		// Schritt 9
+		moveMilimetersBlocking(-500, speed);
 		
 		System.exit(0);
 	}
 
+	// ROTATION mittels eines Motors mit Drehachse am fixen Rad
 	// deg>0 => rechtsdrehung
 	public static void rotateDegreeBlocking(int degree, int speed) {
 		double distCirc = Math.abs(degree) * AXLE_RADIUS * 2 * Math.PI / 360;
@@ -52,14 +92,51 @@ public class MoveTests {
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			Motor.A.stop();
+			Motor.B.stop();
+			System.out.println("Motor A: " + Motor.A.getTachoCount() + "°\nMotor B: " + Motor.B.getTachoCount() + "°");
 		}
-		Motor.A.stop();
-		Motor.B.stop();
+	}
+	
+	// ROTATION mittels beider Motoren mit Drehachse in der Mitte der Räder
+	// deg>0 => rechtsdrehung
+	public static void rotateSimultaneDegreeBlocking(int degree, int speed) {
+		double distCirc = Math.abs(degree) * AXLE_RADIUS * Math.PI / 360;
+		int deg = (int) (distCirc * 360) / WHEEL_CIRC; // Schneidet ab, statt zu
+													   // runden
+		int driveTime = (int) (distCirc * 360 * 1000) / (speed * WHEEL_CIRC); // leicht
+																				// ungenau
+
+		if (degree > 0) {
+			Motor.A.setSpeed(speed);
+			Motor.B.setSpeed(speed);
+			Motor.A.rotate(-deg, true);
+			Motor.B.rotate(deg, true);
+		} else {
+			Motor.A.setSpeed(speed);
+			Motor.B.setSpeed(speed);
+			Motor.A.rotate(deg, true);
+			Motor.B.rotate(-deg, true);
+		}
+
+		try {
+			Thread.sleep(driveTime); // eventuell laenger laufen lassen, wegen
+								     // Zeitabbruch
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			Motor.A.stop();
+			Motor.B.stop();
+			System.out.println("Motor A: " + Motor.A.getTachoCount() + "°\nMotor B: " + Motor.B.getTachoCount() + "°");
+		}
 	}
 
+	// BEWEGUNG in gerader Richtung
 	public static void moveMilimetersBlocking(int dist, int speed) {
 		int deg = (dist * 360) / WHEEL_CIRC; // Schneidet ab, statt zu runden
-		int driveTime = (dist * 360 * 1000) / (speed * WHEEL_CIRC); // leicht
+		int driveTime = (Math.abs(dist) * 360 * 1000) / (speed * WHEEL_CIRC); // leicht
 																	// ungenau
 
 		Motor.A.setSpeed(speed);
@@ -72,8 +149,11 @@ public class MoveTests {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		Motor.A.stop();
-		Motor.B.stop();
+		finally {
+			Motor.A.stop();
+			Motor.B.stop();
+			System.out.println("Motor A: " + Motor.A.getTachoCount() + "°\nMotor B: " + Motor.B.getTachoCount() + "°");
+		}
 	}
 
 }
