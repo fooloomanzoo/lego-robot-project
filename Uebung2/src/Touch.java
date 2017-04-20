5418c35ebe545a1a8888309206208b9f02751e63
kevkal <kevkal@hotmail.de>
2017-04-20 18:53:00 +0200

    Uebung2

---
+++ Uebung2/src/Touch.java
@@ -0,0 +1,48 @@
+
+import lejos.nxt.Button;
+import lejos.nxt.ButtonListener;
+import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.TouchSensor;
+
+public class Touch {
+	final static int STEP_DEG = 5;    // 2 Grad
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Kroetenwanderung Innen");
+		System.out.println("Druecke Startbutton!");
+		Button.waitForAnyPress();
+		
+		TouchSensor t2 = new TouchSensor(SensorPort.S2);
+		TouchSensor t3 = new TouchSensor(SensorPort.S3);
+		
+		Button.ESCAPE.addButtonListener(new ButtonListener() {
+			   public void buttonPressed(Button b) {
+			      System.exit(0);
+			   }
+
+			   public void buttonReleased(Button b) {
+			      // Nothing here
+			   }
+			});
+		
+		while(true){
+			Motor.A.setSpeed((int) (Move.SPEED_MEDIUM * Move.MOTOR_A_CAL_SPEED));
+			Motor.B.setSpeed((int) (Move.SPEED_MEDIUM * Move.MOTOR_B_CAL_SPEED));
+			
+			Motor.A.forward();
+			Motor.B.forward();
+			
+			if(t2.isPressed()){
+				System.out.println("rechts");
+				Move.rotateSimultaneDegreeBlocking(-STEP_DEG, Move.SPEED_SLOW);
+			}
+			if(t3.isPressed()){
+				System.out.println("links");
+				Move.rotateSimultaneDegreeBlocking(+STEP_DEG, Move.SPEED_SLOW);
+			}	
+		}	
+	}
+
+}
