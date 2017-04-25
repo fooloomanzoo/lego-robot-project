7d272ee1f6759147c13f54a905ee0c97c2ba09ed
kevkal <kevkal@hotmail.de>
2017-04-25 13:56:10 +0200

    Deleted things

--- Uebung2/src/KroeteAussen.java
+++
@@ -1,8 +0,0 @@
-
-public class KroeteAussen {
-
-	public static void main(String[] args) {
-		WalkAlong.start(false);
-	}
-
-}
--- Uebung2/src/KroeteInnen.java
+++
@@ -1,8 +0,0 @@
-
-public class KroeteInnen {
-
-	public static void main(String[] args) {
-		WalkAlong.start(true);
-	}
-
-}
